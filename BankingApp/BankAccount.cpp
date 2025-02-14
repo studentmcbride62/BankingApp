c20152fd7a08c72e740a21f63e7ca41a1be8af4f
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2025-02-14 11:59:15 -0500

    Initial Commit

---
+++ BankingApp/BankAccount.cpp
@@ -0,0 +1,27 @@
+#include "BankAccount.h"
+#include <iostream>
+
+BankAccount::BankAccount(std::string account_number, std::string account_holder_name, double initial_balance) {
+    m_account_holder_name = account_holder_name;
+    m_account_number = account_number;
+    m_balance = initial_balance;
+}
+
+void BankAccount::deposit(double amount) {
+    m_balance += amount;
+}
+void BankAccount::withdraw(double amount) {
+    if (amount > m_balance-5.00) {
+        std::cout << "\nInsufficient funds to withdraw $" << amount << std::endl;
+    }
+    else {
+        m_balance -= amount;
+    }
+}
+void BankAccount::display() {
+    std::cout << "\nAccount Number: " << m_account_number << std::endl;
+    std::cout << "Account Holder: " << m_account_holder_name << std::endl;
+    std::cout.setf(std::ios::fixed);
+    std::cout.precision(2);
+    std::cout << "Balance: $" << m_balance << std::endl;
+}
---
+++ BankingApp/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "BankAccount.h"
+
+int main() {
+    BankAccount accountA("52572480","Brian McBride",2575.50);
+    accountA.display();
+    return 0;
+}
